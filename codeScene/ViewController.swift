b3aa79dfade06a2bbd2cb4ffe0bf1cef38255b62
Shogo <vanitas710@yahoo.co.jp>
2021-01-10 02:28:44 +0900

    コードだけでElCapitanのページに移動できるようにした

--- codeScene/ViewController.swift
+++ codeScene/ViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    @IBAction func gotoElCapitan(_ sender: Any) {
+//        
+//        // 移動先のビューコントローラを参照する
+//        let nextViewControler = self.storyboard?.instantiateViewController(withIdentifier: "ElCapitanPage")
+//        
+//        // トランジションの映像効果を指定する
+//        nextViewControler?.modalTransitionStyle = .flipHorizontal
+//        
+//        // シーンを移動する
+//        present(nextViewControler!, animated: true, completion: nil)
+//        
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
